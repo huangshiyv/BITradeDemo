fcc07249013c1db653dddfa5d3b7ef70b8a5c657
HUANG <shiyu.huang@sap.com>
2019-05-31 14:03:12 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AmplifyAngularModule, AmplifyIonicModule, AmplifyService } from 'aws-amplify-angular';
 import { HomeComponent } from './modules/home/pages/home/home.component';
 import { AuthComponent } from './core/authentication/auth/auth.component';
 import { SecureComponent } from './modules/home/pages/secure/secure.component';
 import { LoginComponent } from './core/authentication/login/login.component';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-
+import {MatButtonModule, MatInputModule,
+  MatCheckboxModule, MatFormFieldModule, MatCardModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,11 +24,14 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    ClarityModule,
     BrowserAnimationsModule,
     AmplifyAngularModule,
     AmplifyIonicModule,
-    MatButtonModule, MatCheckboxModule
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [AmplifyService],
   bootstrap: [AppComponent]
